b79efce6f817d5f346d679bc785c61abcfe6799f
Tertius Erskine <terersk@gmail.com>
2023-02-14 12:46:05 -0500

    Seperated lab folders

---
+++ Lab2/src/main/java/com/cpan252/CpanTekken/CpanTekkenApplication.java
@@ -0,0 +1,13 @@
+package com.cpan252.CpanTekken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpanTekkenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CpanTekkenApplication.class, args);
+	}
+
+}
