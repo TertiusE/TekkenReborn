fcdbdc4a74c9534da2de5bd396ba1229e1087b54
Tertius Erskine <terersk@gmail.com>
2023-02-18 23:29:40 -0500

    Lab 2 Complete

--- Lab2/src/main/java/com/cpan252/CpanTekken/controller/DesignController.java
+++ Lab2/src/main/java/com/cpan252/CpanTekken/controller/DesignController.java
@@ -3,18 +3,21 @@ import java.util.EnumSet;
 import java.util.stream.Collectors;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.SessionAttributes; 
 import com.cpan252.CpanTekken.model.Fighter.Anime;
 import com.cpan252.CpanTekken.model.Fighter;
-
+import com.cpan252.CpanTekken.model.FighterPool;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @Slf4j
+@SessionAttributes("fighterPool")
 @RequestMapping("/design")
 public class DesignController {
     @GetMapping
@@ -24,14 +27,28 @@ public class DesignController {
 
     @ModelAttribute
     public void animes(Model model) {
-        var animes = EnumSet.allOf(Anime.class).stream()
-            .map(Anime::getTitle).collect(Collectors.toList());
+        var animes = EnumSet.allOf(Anime.class).stream().collect(Collectors.toList());
         model.addAttribute("animes",animes);
         log.info("anime {}", animes);
+    }
+
+    @ModelAttribute(name = "fighterPool")
+    public FighterPool fighterPool() {
+        return new FighterPool();
     }
 
     @ModelAttribute
     public Fighter fighter() {
         return Fighter.builder().build();
+    }
+
+    @PostMapping
+    public String processFighterAddition(Fighter fighter, @ModelAttribute FighterPool pool, Errors errors) {
+        if (errors.hasErrors()){
+            return "design";  
+        }
+        System.out.println(fighter);
+        pool.add(fighter);
+        return "redirect:/design";
     }
 }
--- Lab2/src/main/java/com/cpan252/CpanTekken/model/Fighter.java
+++ Lab2/src/main/java/com/cpan252/CpanTekken/model/Fighter.java
@@ -1,15 +1,31 @@
 package com.cpan252.CpanTekken.model;
 
+import java.math.BigDecimal;
+
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
 public class Fighter {
+    @NotBlank
     private String name;
+    @NotBlank
+    @Max(100)
     private int damagePerHit;
-    private int health;
-    private double resistance;
+    @NotBlank
+    @Min(1000)
+    private double health;
+    @NotBlank
+    @DecimalMin(value = "0.1", inclusive = true)
+    @DecimalMax(value = "10.0", inclusive = true)
+    private BigDecimal resistance;
+    @NotBlank
     private Anime animeFrom;
 
     public enum Anime {
