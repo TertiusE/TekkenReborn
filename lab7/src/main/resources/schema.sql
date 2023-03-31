af042cb3fc099f9030c680fa112fbccd80a51279
Tertius Erskine <terersk@gmail.com>
2023-03-31 16:30:02 -0400

    .

--- lab7/src/main/resources/schema.sql
+++ lab7/src/main/resources/schema.sql
@@ -1,9 +1,9 @@
 create table if not exists fighter (
-    id identity,
-    name varchar2(50) not null,
-    damage_per_hit int not null,
-    health int not null,
-    resistance double not null,
-    anime_from varchar2(50) not null,
-    created_at timestamp not null
+  id identity,
+  name varchar(50) not null,
+  damage_per_hit int not null,
+  health int not null,
+  resistance double not null,
+  anime_from varchar(50) not null,
+  created_at timestamp not null
 );
